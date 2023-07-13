34d1c77c2d9e9d3ade22ed9354a81c0540df7fd8
sonukuldeep <sonukuldeep@outlook.com>
2023-07-13 20:13:53 +0530

    base

---
+++ README.md
@@ -0,0 +1,50 @@
+
+# Particle animation template
+
+This repo is a template to all animation related to particles.
+
+This is inspired by Frank's [video](https://www.youtube.com/watch?app=desktop&v=XGioNBHrFU4&feature=youtu.be)
+
+A heartly thanks to Frank, the creator of the tutorial and many similar tutorial on canvas element and animation. Please do which his channel
+
+## Acknowledgements
+
+ - Frank laboratory on youtuber
+ - My brain
+ - Every tutorial video out there
+ - Opensource community
+
+
+## Installation
+
+### The project requires 80's laptop.
+
+### Clone the website using
+```npm
+git clone 
+```
+
+### Nothing to install
+
+### Run using vs code live server
+
+### In the browser open
+
+```bash
+  http://localhost:5500/
+```
+
+## Badges
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/) 
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+
+
+## Author
+- [@sonukuldeep](https://www.github.com/sonukuldeep)
+
+
+## 🛠 Skills
+
+[![My Skills](https://skillicons.dev/icons?i=js,ts,html,css,tailwind,sass,nodejs,react,nextjs,svelte,vue,flask,rust,python,php,solidity,mongodb,mysql,prisma,figma,threejs,unity,godot)](https://github.com/sonukuldeep)
---
+++ script.js
@@ -0,0 +1,76 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const fps = 30
+const interval = 1000 / fps
+let lastTime = 0
+
+let particlesArray = []
+
+const mouse = {
+    x: null,
+    y: null,
+    radius: 25000
+}
+
+window.addEventListener('mousemove', (e) => {
+    mouse.x = e.x
+    mouse.y = e.y
+})
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y
+        this.size = 3
+        this.baseX = this.x
+        this.baseY = this.y
+        this.density = (Math.random() * 30) + 1
+    }
+
+    draw() {
+        ctx.fillStyle = 'white'
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+        ctx.closePath()
+        ctx.fill()
+    }
+
+    update() {
+        const dx = mouse.x - this.x
+        const dy = mouse.y - this.y
+        const distance = dx * dx + dy * dy
+        if (distance < mouse.radius)
+            this.size = 5
+        else
+            this.size = 3
+    }
+}
+
+
+function inti() {
+    for (let index = 0; index < 500; index++) {
+        const randomX = Math.floor(Math.random() * canvas.width)
+        const randomY = Math.floor(Math.random() * canvas.height)
+        particlesArray.push(new Particle(randomX, randomY))
+    }
+}
+
+inti()
+
+function animate(timestamp) {
+    const elapsedTime = timestamp - lastTime
+    if (elapsedTime > interval) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        for (let i = 0; i < particlesArray.length; i++) {
+            particlesArray[i].draw()
+            particlesArray[i].update()
+        }
+        lastTime = timestamp
+    }
+    requestAnimationFrame(animate)
+}
+
+animate(0)
